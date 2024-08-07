b274e63b52a21a8dc73af16df1f0d067c21d6bd8
Vitor-Moura48 <gabriel.4815.7@gmail.com>
2024-08-06 22:44:42 -0300

    Adicionando classe para as funções

--- 1/main.py
+++ 1/main.py
@@ -1,47 +1,51 @@
-def verificar_triangulo(lados):
-    lados_ordenados = sorted(lados)
-   
-    if lados_ordenados[0] + lados_ordenados[1] <= lados_ordenados[2]:
-        print('Triângulo Inválido')
-        return False
-    return True
+class Triangulo:
+    def __init__(self, lados) -> None:
+        self.lados = lados
 
-def area_triangulo(lados):
-    semi_perimetro = sum(lados) / 2
-    return round( (semi_perimetro * (semi_perimetro-lados[0]) * (semi_perimetro-lados[1]) * (semi_perimetro-lados[2]))**0.5 , 2)
+    def verificar_triangulo(self):
+        lados_ordenados = sorted(self.lados)
+    
+        if len(self.lados) != 3 or lados_ordenados[0] + lados_ordenados[1] <= lados_ordenados[2]:
+            print('Triângulo Inválido')
+            return False
+        return True
 
-def forma_triangulo(lados):
-    combinacoes = len(set(lados))
-    match combinacoes:
-        case 1:
-            return "Equilátero"
-        case 2:
-            return "Isoceles"
-        case 3:
-            return "Escaleno"
+    def area_triangulo(self):
+        semi_perimetro = sum(self.lados) / 2
+        return round( (semi_perimetro * (semi_perimetro-self.lados[0]) * (semi_perimetro-self.lados[1]) * (semi_perimetro-self.lados[2]))**0.5 , 2)
 
+    def forma_triangulo(self):
+        combinacoes = len(set(self.lados))
+        match combinacoes:
+            case 1:
+                return "Equilátero"
+            case 2:
+                return "Isoceles"
+            case 3:
+                return "Escaleno"
 
 def main():
     rodando = True
     while rodando:
         print("\n*** Informe os lados do triângulo [0 = Sair] ***\n")
-        triangulo = []
+        lados = []
         
-        while(len(triangulo)) < 3:
+        while(len(lados)) < 3:
             try:
-                lado = float( input(f"Lado {len(triangulo) + 1}: ") )
+                lado = float( input(f"Lado {len(lados) + 1}: ") )
                 if lado >= 0:
                     if lado == 0:
                         rodando = False
                         break
-                    triangulo.append(lado)
+                    lados.append(lado)
             except:
                 print("Ecorreu algum erro, tente novamnete!")
-            
-        if len(triangulo) == 3 and verificar_triangulo(triangulo):
+        
+        triangulo = Triangulo(lados)
+        if triangulo.verificar_triangulo():
             print()
-            print(f"Area do triângulo: {area_triangulo(triangulo)}")
-            print(f"Forma do triângulo: {forma_triangulo(triangulo)}")
+            print(f"Area do triângulo: {triangulo.area_triangulo()}")
+            print(f"Forma do triângulo: {triangulo.forma_triangulo()}")
     
     print("\nEncerrando Programa")
 
